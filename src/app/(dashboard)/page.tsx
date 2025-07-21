18ac03311249372efcef47450435124af7348b6d
Arun <arunonwork3249@gmail.com>
2025-07-21 12:47:22 +0530

    Fixed Auth

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,6 +1,19 @@
 // app/(dashboard)/page.tsx
+"use client";
+import { useEffect } from "react";
 import Main from "../../../components/Main";
+import { useRouter } from "next/navigation";
 
 export default function HomePage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("auth_token");
+    if (!token) {
+      // Redirect to login if no token
+      router.push("/auth");
+    }
+  }, []);
+
   return <Main />;
 }
